6fb2a3de59dbf7c36bb31aa6029cfca45b8c4785
Franco Meloni <franco.meloni91@gmail.com>
2018-09-04 16:34:10 +0100

    Moved the template string outside the class to be static

--- Sources/CapriccioLib/SwiftTestCodeGenerator.swift
+++ Sources/CapriccioLib/SwiftTestCodeGenerator.swift
@@ -8,7 +8,6 @@
 import Gherkin
 import Stencil
 
-final class SwiftTestCodeGenerator {
 protocol SwiftTestCodeGenerating {
     func generateSwiftTestCode(forFeature feature: Feature) -> String
 }
@@ -24,30 +23,30 @@ final class SwiftTestCodeGenerator: SwiftTestCodeGenerating {
             fatalError("Template file rendering failed with error \(error)")
         }
     }
-    
-    /// Simple way of embedding the template given SPM doesn't support resources files yet
-    let templateString = """
-    import XCTest
-    import XCTest_Gherkin
+}
 
-    final class {{ feature.className }} {
-        {% for scenario in feature.scenarios %}
-        {% if scenario.examples.count > 0 %}
-        {% for i in 0...scenario.examplesCountForIteration %}
-        func {{ scenario.methodName }}With{{ scenario.examples[i].methodNameExamplePart }} {
-            {% for step in scenario.examplesSteps[i] %}
-            {{ step.swiftText }}
-            {% endfor %}
-        }
-        {% endfor %}
-        {% else %}
-        func {{ scenario.methodName }} {
-            {% for step in scenario.steps%}
-            {{ step.swiftText }}
-            {% endfor %}
-        }
-        {% endif %}
+/// Simple way of embedding the template given SPM doesn't support resources files yet
+private let templateString = """
+import XCTest
+import XCTest_Gherkin
+
+final class {{ feature.className }} {
+    {% for scenario in feature.scenarios %}
+    {% if scenario.examples.count > 0 %}
+    {% for i in 0...scenario.examplesCountForIteration %}
+    func {{ scenario.methodName }}With{{ scenario.examples[i].methodNameExamplePart }} {
+        {% for step in scenario.examplesSteps[i] %}
+        {{ step.swiftText }}
+    {% endfor %}
+    }
+    {% endfor %}
+    {% else %}
+    func {{ scenario.methodName }} {
+        {% for step in scenario.steps%}
+        {{ step.swiftText }}
         {% endfor %}
     }
-    """
+    {% endif %}
+    {% endfor %}
 }
+"""
