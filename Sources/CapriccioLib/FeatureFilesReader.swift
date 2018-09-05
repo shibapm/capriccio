49693880825db35b02f4ca0f2f8ea3d23105f3b6
Franco Meloni <franco.meloni91@gmail.com>
2018-09-05 09:44:02 +0100

    Added support for included and excluded tags

--- Sources/CapriccioLib/FeatureFilesReader.swift
+++ Sources/CapriccioLib/FeatureFilesReader.swift
@@ -9,8 +9,28 @@ import Foundation
 import Gherkin
 
 final class FeatureFilesReader {    
-    func readFiles(atPaths paths: [String]) -> [Feature] {
+    func readFiles(atPaths paths: [String], includedTags: [String]?, excludedTags: [String]?) -> [Feature] {
         let filesContent = paths.compactMap { try? String(contentsOfFile: $0) }
-        return filesContent.compactMap { try? Feature($0) }
+        let features = filesContent.compactMap { try? Feature($0) }
+        
+        if includedTags != nil ||
+            excludedTags != nil {
+            return filterFeatures(features: features, includedTags: includedTags, excludedTags: excludedTags)
+        } else {
+            return features
+        }
+    }
+    
+    private func filterFeatures(features: [Feature], includedTags: [String]?, excludedTags: [String]?) -> [Feature] {
+        let includedTags = includedTags ?? []
+        let excludedTags = excludedTags ?? []
+        
+        return features.compactMap { feature -> Feature? in
+            let scenarios = feature.scenarios.filter { scenario in
+                scenario.tags.contains { includedTags.contains($0.tagName) } && !scenario.tags.contains { excludedTags.contains($0.tagName) }
+            }
+            
+            return scenarios.count == 0 ?  nil : Feature(name: feature.name, description: feature.textDescription, scenarios: scenarios)
+        }
     }
 }
--- Tests/CapriccioLibTests/FeatureFilesReaderTests.swift
+++ Tests/CapriccioLibTests/FeatureFilesReaderTests.swift
@@ -11,11 +11,6 @@ import Nimble
 @testable import CapriccioLib
 
 final class FeatureFilesReaderTests: XCTestCase {
-    let testFile1Path = NSSearchPathForDirectoriesInDomains(.applicationDirectory,
-                                                            .userDomainMask, true).first! + "test1.feature"
-    let testFile2Path = NSSearchPathForDirectoriesInDomains(.applicationDirectory,
-                                                            .userDomainMask, true).first! + "test2.feature"
-    
     var featureFilesReader: FeatureFilesReader!
     
     override func setUp() {
@@ -27,33 +22,74 @@ final class FeatureFilesReaderTests: XCTestCase {
         featureFilesReader = nil
         try? FileManager.default.removeItem(atPath: testFile1Path)
         try? FileManager.default.removeItem(atPath: testFile2Path)
+        try? FileManager.default.removeItem(atPath: testFile3Path)
         super.tearDown()
     }
     
     func testItCanReadSingleFeatureFile() {
-        try! testFeatureFile1Content().write(toFile: testFile1Path, atomically: true, encoding: .utf8)
+        write(fileContent: testFeatureFile1Content(), toPath: testFile1Path)
         
-        let featureFiles = featureFilesReader.readFiles(atPaths: [testFile1Path])
+        let featureFiles = featureFilesReader.readFiles(atPaths: [testFile1Path], includedTags: nil, excludedTags: nil)
         
         expect(featureFiles.count) == 1
         expect(featureFiles[0].name) == "Test Scenario"
     }
     
     func testItCanReadMultipleFeatureFiles() {
-        try! testFeatureFile1Content().write(toFile: testFile1Path, atomically: true, encoding: .utf8)
-        try! testFeatureFile2Content().write(toFile: testFile2Path, atomically: true, encoding: .utf8)
+        write(fileContent: testFeatureFile1Content(), toPath: testFile1Path)
+        write(fileContent: testFeatureFile2Content(), toPath: testFile2Path)
         
-        let featureFiles = featureFilesReader.readFiles(atPaths: [testFile1Path, testFile2Path])
+        let featureFiles = featureFilesReader.readFiles(atPaths: [testFile1Path, testFile2Path], includedTags: nil, excludedTags: nil)
         
         expect(featureFiles.count) == 2
         expect(featureFiles[0].name) == "Test Scenario"
         expect(featureFiles[1].name) == "Test Scenario 2"
     }
     
+    func testItCanReadMultipleFeatureFilesWithIncludedTags() {
+        write(fileContent: testFeatureFile1Content(), toPath: testFile1Path)
+        write(fileContent: testFeatureFile2Content(), toPath: testFile2Path)
+        write(fileContent: testFeatureFile3Content(), toPath: testFile3Path)
+        
+        let featureFiles = featureFilesReader.readFiles(atPaths: [testFile1Path, testFile2Path, testFile3Path], includedTags: ["included"], excludedTags: nil)
+        
+        expect(featureFiles.count) == 2
+        expect(featureFiles[0].name) == "Test Scenario 2"
+        expect(featureFiles[1].name) == "Test Scenario 3"
+    }
+    
+    func testItCanReadMultipleFeatureFilesWithIncludedAndExcludedTags() {
+        write(fileContent: testFeatureFile1Content(), toPath: testFile1Path)
+        write(fileContent: testFeatureFile2Content(), toPath: testFile2Path)
+        write(fileContent: testFeatureFile3Content(), toPath: testFile3Path)
+        
+        let featureFiles = featureFilesReader.readFiles(atPaths: [testFile1Path, testFile2Path, testFile3Path], includedTags: ["included"], excludedTags: ["excluded"])
+        
+        expect(featureFiles.count) == 1
+        expect(featureFiles[0].name) == "Test Scenario 2"
+    }
+}
+
+private extension FeatureFilesReaderTests {
+    private var testFile1Path: String {
+        return NSSearchPathForDirectoriesInDomains(.applicationDirectory,
+                                                   .userDomainMask, true).first! + "test1.feature"
+    }
+    
+    private var testFile2Path: String {
+        return NSSearchPathForDirectoriesInDomains(.applicationDirectory,
+                                                            .userDomainMask, true).first! + "test2.feature"
+    }
+    
+    private var testFile3Path: String {
+        return NSSearchPathForDirectoriesInDomains(.applicationDirectory,
+                                                   .userDomainMask, true).first! + "test3.feature"
+    }
+    
     private func testFeatureFile1Content() -> String {
         return """
         Feature: Test Scenario
-
+        
         Scenario: test scenario
         Given I have to do a test
         And I'm a developer
@@ -64,9 +100,25 @@ final class FeatureFilesReaderTests: XCTestCase {
         return """
         Feature: Test Scenario 2
         
+        @included
         Scenario: test scenario 2
         Given I have to do another test
         And I'm the same developer i was before
         """
+    }
+    
+    private func testFeatureFile3Content() -> String {
+        return """
+        Feature: Test Scenario 3
+        
+        @included @excluded
+        Scenario: test scenario 3
+        Given I have to do another test
+        And I will use it just for tags
+        """
+    }
+    
+    private func write(fileContent: String, toPath path: String) {
+        try! fileContent.write(toFile: path, atomically: true, encoding: .utf8)
     }
 }
